3d31fd86a6d1fd5d3dc146988113c72fa6e199b7
Alexey Kotlyarov <a@koterpillar.com>
2021-10-03 15:40:01 +1100

    Gnome Terminal

--- Export.hs
+++ Export.hs
@@ -3,12 +3,15 @@
 
 module Export
   ( module Export.ITerm
+  , module Export.GnomeTerminal
   , export
   ) where
 
 import           ColorScheme
+import           Export.GnomeTerminal
 import           Export.ITerm
 
 export :: ColorScheme -> IO ()
 export cs = do
+  exportGnomeTerminal cs
   exportITerm cs
---
+++ Export/GnomeTerminal.hs
@@ -0,0 +1,12 @@
+module Export.GnomeTerminal
+  ( formatGnomeTerminal
+  , exportGnomeTerminal
+  ) where
+
+import           ColorScheme
+
+formatGnomeTerminal :: ColorScheme -> String
+formatGnomeTerminal = error "Not implemented"
+
+exportGnomeTerminal :: ColorScheme -> IO ()
+exportGnomeTerminal = error "Not implemented"
