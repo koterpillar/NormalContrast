59d239da62760dc4849c146ffcbeb035b9743ebb
Alexey Kotlyarov <a@koterpillar.com>
2022-01-23 19:12:53 +1100

    Add GNOME

--- ColorScheme/Stock.hs
+++ ColorScheme/Stock.hs
@@ -4,33 +4,53 @@ import           AnsiColor
 import           Color
 import           ColorScheme
 
-windowsXP :: ColorScheme
-windowsXP =
-  ColorScheme
-    { csName = "Windows XP"
-    , csBackground = black
-    , csForeground = white
-    , csCursor = white
-    , csCursorText = black
-    , csColor =
-        \case
-          Normal Black   -> Color 0 0 0
-          Normal Red     -> Color 128 0 0
-          Normal Green   -> Color 0 128 0
-          Normal Yellow  -> Color 128 128 0
-          Normal Blue    -> Color 0 0 128
-          Normal Magenta -> Color 128 0 128
-          Normal Cyan    -> Color 0 128 128
-          Normal White   -> Color 192 192 192
-          Bright Black   -> Color 128 128 128
-          Bright Red     -> Color 255 0 0
-          Bright Green   -> Color 0 255 0
-          Bright Yellow  -> Color 255 255 0
-          Bright Blue    -> Color 0 0 255
-          Bright Magenta -> Color 255 0 255
-          Bright Cyan    -> Color 0 255 255
-          Bright White   -> Color 255 255 255
-    }
+stockDark :: String -> (AnsiColor -> Color) -> ColorScheme
+stockDark csName csColor = ColorScheme {..}
   where
+    csBackground = black
+    csForeground = white
+    csCursor = white
+    csCursorText = black
     black = Color 0 0 0
     white = Color 255 255 255
+
+windowsXP :: ColorScheme
+windowsXP =
+  stockDark "Windows XP" $ \case
+    Normal Black   -> Color 0 0 0
+    Normal Red     -> Color 128 0 0
+    Normal Green   -> Color 0 128 0
+    Normal Yellow  -> Color 128 128 0
+    Normal Blue    -> Color 0 0 128
+    Normal Magenta -> Color 128 0 128
+    Normal Cyan    -> Color 0 128 128
+    Normal White   -> Color 192 192 192
+    Bright Black   -> Color 128 128 128
+    Bright Red     -> Color 255 0 0
+    Bright Green   -> Color 0 255 0
+    Bright Yellow  -> Color 255 255 0
+    Bright Blue    -> Color 0 0 255
+    Bright Magenta -> Color 255 0 255
+    Bright Cyan    -> Color 0 255 255
+    Bright White   -> Color 255 255 255
+
+-- https://github.com/GNOME/gnome-terminal/blob/51994fdb014e78f5ef2aeac07eed0f71ac30dd31/src/profile-editor.cc#L226
+gnome :: ColorScheme
+gnome =
+  stockDark "GNOME" $ \case
+    Normal Black   -> Color 0x17 0x14 0x21
+    Normal Red     -> Color 0xc0 0x1c 0x28
+    Normal Green   -> Color 0x26 0xa2 0x69
+    Normal Yellow  -> Color 0xa2 0x73 0x4c
+    Normal Blue    -> Color 0x12 0x48 0x8b
+    Normal Magenta -> Color 0xa3 0x47 0xba
+    Normal Cyan    -> Color 0x2a 0xa1 0xb3
+    Normal White   -> Color 0xd0 0xcf 0xcc
+    Bright Black   -> Color 0x5e 0x5c 0x64
+    Bright Red     -> Color 0xf6 0x61 0x51
+    Bright Green   -> Color 0x33 0xd1 0x7a
+    Bright Yellow  -> Color 0xe9 0xad 0x0c
+    Bright Blue    -> Color 0x2a 0x7b 0xde
+    Bright Magenta -> Color 0xc0 0x61 0xcb
+    Bright Cyan    -> Color 0x33 0xc7 0xde
+    Bright White   -> Color 0xff 0xff 0xff
